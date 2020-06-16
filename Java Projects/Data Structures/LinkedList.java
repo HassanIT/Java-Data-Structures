0d2e7427802a308ee72e3b1d930194c88de8d1aa
Hassan Nisar <hassan82@my.yorku.ca>
2020-06-16 19:08:51 -0400

    completion of the linkedlist, and singlylinkedlist data structure classes

--- Java Projects/Data Structures/LinkedList.java
+++ Java Projects/Data Structures/LinkedList.java
@@ -1,28 +1,73 @@
+/**
+ * Class which represents the linkedlist data structure, implemented in java.
+ */
+
 public class LinkedList<E> {
-    private static class Node<E> {
+    public Node<E> head; // pointer to the first element in the list.
+    public Node<E> tail; //pointer to the last element in the list.
+    public int size; //variable to store the size of the list.
+
+/**
+ * Method to check the current size of the list.
+ * @return the current size of the list.
+ */
+    public int size() {
+        return size;
+    }
+    
+/**
+ * Method to check if the list is empty.
+ * @return true if the list is empty, false otherwise.
+ */
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+/**
+ * Class which represents the head, tail, and element nodes in the list.
+ * @param <E> the element type.
+ */
+    public static class Node<E> {
         E element;
         private Node<E> next = null;
 
+/**
+ * Method which sets the node.
+ * @param e the element itself.
+ * @param n the reference to the next element.
+ */
         public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
 
+/**
+ * Method to get the current element.
+ * @return the current element.
+ */
         public E getElement() {
             return element;
         }
 
+/**
+ * Method to get the reference of the next element in the list.
+ * @return the reference to the next element in the list.
+ */
         public Node<E> getNext() {
             return next;
         }
 
+/**
+ * Method to set the reference to the next element in the list.
+ * @param n the reference to set to the next element in the list.
+ */
         public void setNext(Node<E> n) {
             next = n;
         }
-    }
-
-    public Node<E> head;
-    public Node<E> tail;
-    public int size;
-    
+    }   
 }
--- Java Projects/Data Structures/SinglyLinkedList.java
+++ Java Projects/Data Structures/SinglyLinkedList.java
@@ -1,49 +1,81 @@
-public class SinglyLinkedList<E> extends LinkedList<E> {
-    /**
- * constructor for creating a singlylinkedlist.
+/**
+ * Class which represents the singlylinkedlist data structure, implemented in java. Extends the base or parent class LinkedList.
  */
-public SinglyLinkedList() {
+public class SinglyLinkedList<E> extends LinkedList<E> {
+
+ /**
+  * Default constructor which creates an empty singlylinkedlist.
+  */
+    public SinglyLinkedList() {
     head = null;
     tail = null;
     size = 0;
-    
-}
+    }
 
 /**
-* 
-* @return the size of the linkedlist.
-*/
-public int size() {
-
-}
+ * Method to see if a 'head' element exists.
+ * @return the first element present in the singlylinkedlist, null otherwise.
+ */
+    public E first() {
+        if (head != null) {
+            return head.getElement();
+        }
+        else {
+            return null;
+        }
+    }
 
 /**
-* 
-* @return true if the linkedlist is empty, false otherwise.
-*/
+ * Method to see if a 'tail' element exists.
+ * @return the last element present in the singlylinkedlist, null otherwise.
+ */
+    public E last() {
+        if (tail != null) {
+            return tail.getElement();
+        }
+        else {
+            return null;
+        }
+    }
 
-public boolean isEmpty() {
+/**
+ * Method to add an element to the beginning of the singlylinkedlist.
+ * @param element to add to the beginning of the singlylinkedlist.
+ */
+    public void addFisrt(E element) {
+        head = new Node<E>(element, head);
+        if (isEmpty()) {
+            tail = head;
+        }
+        size++;
+    }
 
-}
+/**
+ * Method to add an element to the end of the singlylinkedlist.
+ * @param element to add to the end of the singlylinkedlist.
+ */
+    public void addLast(E element) {
+        tail = new Node<E>(element, tail);
+        if (isEmpty()) {
+            head = tail;
+        }
+        size++;
+    }
 
-public E first() {
-
-}
-
-public E list() {
-
-}
-
-public void addFisrt(E element) {
-
-}
-
-public void addLast(E element) {
-
-}
-
-public E removeFirst() {
-
-}
-
-}
+/**
+ * Method to remove the first element of the singlylinkedlist.
+ * @return the element removed from the beginning of the singlylinkedlist.
+ */
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        E element = head.getElement();
+        head = head.getNext();
+        size--;
+        if (size == 0) {
+            tail = null;
+        }
+        return element;
+        }
+    }

b95a7d295464eddcb1ee3b31ef17c0dc1e5181cb
Hassan Nisar <hassan82@my.yorku.ca>
2020-06-16 19:45:55 -0400

    initial singlylinkedqueue

--- Java Projects/Data Structures/Linear/LinkedListQueue.java
+++ Java Projects/Data Structures/Linear/LinkedListQueue.java
@@ -2,5 +2,42 @@
  * Class which represents the queue data structure in java.
  */
 public class LinkedListQueue<E> implements Queue<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<E>();
     
+    public LinkedListQueue() {
+        list.head = null;
+        list.tail = null;
+        list.size = 0;
+    }
+
+    public int size() {
+        return list.size;
+    }
+
+    public boolean isEmpty() {
+        if (list.size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public E first() {
+        return list.tail;
+    }
+
+    public void enqueue(E node) {
+        list.addLast(node);
+    }
+
+    public E dequeue() {
+        if (list.head == null) {
+            list.tail = null;
+            return null;
+        }
+        else {
+            return list.removeFirst();
+        }
+    }
 }

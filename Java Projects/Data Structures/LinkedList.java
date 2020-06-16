d96155fd396a7eba35f374dc5c44d89479435452
Hassan Nisar <hassan82@my.yorku.ca>
2020-06-16 16:42:19 -0400

    added empty methods and node class to linkedlist

--- Java Projects/Data Structures/LinkedList.java
+++ Java Projects/Data Structures/LinkedList.java
@@ -1,3 +1,41 @@
-public class LinkedList {
+public class LinkedList<E> {
+    private static class Node<E> {
+
+    }
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
     
+    public SinglyLinkedList() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public E first() {
+
+    }
+
+    public E list() {
+
+    }
+
+    public void addFisrt(E element) {
+
+    }
+
+    public void addLast(E element) {
+
+    }
+
+    public E removeFirst() {
+        
+    }
+
 }

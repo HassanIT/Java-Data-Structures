99ac6d0843bea1917eba60c286d0fd5b768b012f
Hassan Nisar <hassan82@my.yorku.ca>
2020-06-16 16:50:30 -0400

    implemented node class

--- Java Projects/Data Structures/LinkedList.java
+++ Java Projects/Data Structures/LinkedList.java
@@ -1,18 +1,48 @@
 public class LinkedList<E> {
     private static class Node<E> {
+        E element;
+        private Node<E> next = null;
 
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
+        }
+
+        public E getNextElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
     }
     private Node<E> head;
     private Node<E> tail;
     private int size;
     
+/**
+ * constructor for creating a singlylinkedlist.
+ */
     public SinglyLinkedList() {
 
     }
 
+/**
+ * 
+ * @return the size of the linkedlist.
+ */
     public int size() {
 
     }
+
+    /**
+     * 
+     * @return true if the linkedlist is empty, false otherwise.
+     */
 
     public boolean isEmpty() {
 
@@ -35,7 +65,7 @@ public class LinkedList<E> {
     }
 
     public E removeFirst() {
-        
+
     }
 
 }

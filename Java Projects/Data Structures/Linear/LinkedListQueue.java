aa284572caeee0cf20c5a1daa6004c45c75b51c0
Hassan Nisar <hassan82@my.yorku.ca>
2020-06-16 20:34:55 -0400

    completed linkedlistqueue class

--- Java Projects/Data Structures/Linear/LinkedListQueue.java
+++ Java Projects/Data Structures/Linear/LinkedListQueue.java
@@ -2,18 +2,29 @@
  * Class which represents the queue data structure in java.
  */
 public class LinkedListQueue<E> implements Queue<E> {
-    private SinglyLinkedList<E> list = new SinglyLinkedList<E>();
+    private SinglyLinkedList<E> list = new SinglyLinkedList<E>(); //Creates a singlylinkedlist
     
+/**
+ * Constructor for creating an empty queue.
+ */
     public LinkedListQueue() {
         list.head = null;
         list.tail = null;
         list.size = 0;
     }
 
+/**
+ * Method to return the size of the queue.
+ * @return the size of the queue.
+ */
     public int size() {
         return list.size;
     }
 
+/**
+ * Method to check if the queue is empty.
+ * @return true if the queue is empty, false otherwise.
+ */
     public boolean isEmpty() {
         if (list.size == 0) {
             return true;
@@ -23,17 +34,32 @@ public class LinkedListQueue<E> implements Queue<E> {
         }
     }
 
+/**
+ * Method to get the first element in the queue.
+ * @return the first element in the queue.
+ */
     public E first() {
-        return list.tail;
+        if (isEmpty()) {
+            return null;
+        }
+        //need to implement the else part.
+        return null;
     }
 
+/**
+ * Method to add an element to the end of the queue.
+ * @param E the element to add to the end of the queue.
+ */
     public void enqueue(E node) {
         list.addLast(node);
     }
 
+/**
+ * Method to remove an element from the queue.
+ * @param E the element to remove from the queue. 
+ */
     public E dequeue() {
-        if (list.head == null) {
-            list.tail = null;
+        if (isEmpty()) {
             return null;
         }
         else {
